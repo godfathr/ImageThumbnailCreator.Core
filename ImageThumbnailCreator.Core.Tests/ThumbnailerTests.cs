ceca2b5eee26d0e696eeb89e67044b53fbd92b72
godfathr <mikal.k.grant@outlook.com>
2021-04-14 17:14:25 -0600

    Brain is too tired to do anything else...

--- ImageThumbnailCreator.Core.Tests/ThumbnailerTests.cs
+++ ImageThumbnailCreator.Core.Tests/ThumbnailerTests.cs
@@ -9,6 +9,12 @@ namespace ImageThumbnailCreator.Core.Tests
         private static string _testImageFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestImages\largeLandscape.jpg");
         private static string _thumbnailFolder = Path.Combine(_testImageFolder, @"TestThumbnails");
         private Thumbnailer _thumbnailer = new Thumbnailer();
+        DirectoryFixture _fixture;
+
+        public ThumbnailerTests(DirectoryFixture fixture)
+        {
+            this._fixture = fixture;
+        }
 
         [Fact]
         public void CreateNewThumbnailDirectoryCreatesDirectorySuccessfully()
@@ -25,6 +31,7 @@ namespace ImageThumbnailCreator.Core.Tests
         public void SaveOriginalSavesSuccessfully()
         {
             //setup
+            SetupTestDirectory();
             string imageLocation = Path.Combine(_testImageFolder, @"TestImages\largeLandscape.jpg");
 
             //act
@@ -35,13 +42,31 @@ namespace ImageThumbnailCreator.Core.Tests
             //assert
             Assert.True(images.Length == 1);
             Assert.Single(images);
-        }
-    }
 
-    public class DirectoryFixture : IDisposable
-    {
-        private static string _testImageFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestImages\largeLandscape.jpg");
-        private static string _thumbnailFolder = Path.Combine(_testImageFolder, @"TestThumbnails");
+            //tear down
+            TearDownTestDirectory();
+        }
+
+        private void ConvertFileToStream(string fileName)
+        {
+            var path = Path.Combine(_testImageFolder, fileName);
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+            else
+            {
+                using (FileStream fs = File.OpenRead(path))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    while (fs.Read(b, 0, b.Length) > 0)
+                    {
+                        Console.WriteLine(temp.GetString(b));
+                    }
+                }
+            }
+        }
 
         private void SetupTestDirectory()
         {
@@ -74,10 +99,49 @@ namespace ImageThumbnailCreator.Core.Tests
             }
 
         }
-
-        public void Dispose()
-        {
-            TearDownTestDirectory();
-        }
     }
+
+    //TODO: Figure out why this doesn't work
+    //public class DirectoryFixture : IDisposable
+    //{
+    //    private static string _testImageFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TestImages\largeLandscape.jpg");
+    //    private static string _thumbnailFolder = Path.Combine(_testImageFolder, @"TestThumbnails");
+
+    //    private void SetupTestDirectory()
+    //    {
+    //        try
+    //        {
+    //            if (!Directory.Exists(_thumbnailFolder))
+    //            {
+    //                Directory.CreateDirectory(_thumbnailFolder);
+    //            }
+    //        }
+    //        catch (Exception)
+    //        {
+    //            throw;
+    //        }
+
+    //    }
+
+    //    private void TearDownTestDirectory()
+    //    {
+    //        try
+    //        {
+    //            if (Directory.Exists(_thumbnailFolder))
+    //            {
+    //                Directory.Delete(_thumbnailFolder);
+    //            }
+    //        }
+    //        catch (Exception)
+    //        {
+    //            throw;
+    //        }
+
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        TearDownTestDirectory();
+    //    }
+    //}
 }
